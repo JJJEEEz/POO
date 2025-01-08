8b0c350cde46c2d4e4a05d3aeb8944c11ca568c0
JJJEEEz <juanestebanperezvelazquez@gmail.com>
2025-01-08 13:22:29 -0600

    Creando atributos de la clase

---
+++ poo_2c.py
@@ -0,0 +1,22 @@
+class Personaje:
+    #Atrubutos de la clase
+    nombre = 'Default'
+    fuerza = 0
+    inteligencia = 0
+    defensa = 0
+    vida = 0
+
+#Variable del constructor vacio
+mi_personaje = Personaje()
+#Modificando valores de los atributos
+mi_personaje.nombre = "EstebanDido"
+mi_personaje.fuerza = 300
+mi_personaje.inteligencia = -2
+mi_personaje.defensa = 30
+mi_personaje.vida = 2
+
+print("El nombre de mi personaje es: ", mi_personaje.nombre)
+print("El nombre de mi personaje es: ", mi_personaje.fuerza)
+print("El nombre de mi personaje es: ", mi_personaje.inteligencia)
+print("El nombre de mi personaje es: ", mi_personaje.defensa)
+print("El nombre de mi personaje es: ", mi_personaje.vida)
