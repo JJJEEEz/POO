521aa69366a977c0c236e09a0af04e2e395e8fff
JJJEEEz <juanestebanperezvelazquez@gmail.com>
2025-01-10 13:30:08 -0600

    Clase 2: añadir enemigo, ataque, daño e impresion de atributos

--- poo_2c.py
+++ poo_2c.py
@@ -1,22 +1,67 @@
 class Personaje:
-    #Atrubutos de la clase
-    nombre = 'Default'
-    fuerza = 0
-    inteligencia = 0
-    defensa = 0
-    vida = 0
+    # Atrubutos de la clase
+    # nombre = 'Default'
+    # fuerza = 0
+    # inteligencia = 0
+    # defensa = 0
+    # vida = 0
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
+        self.nombre = nombre
+        self.fuerza = fuerza
+        self.inteligencia = inteligencia
+        self.defensa = defensa
+        self.vida = vida
+
+    def imprimir_atributos(self):
+        print(self.nombre)
+        print("-Fuerza: ", self.fuerza)
+        print("-Inteligencia: ", self.inteligencia)
+        print("-Defensa: ", self.defensa)
+        print("-Vida: ", self.vida)
+    
+    def subir_nivel(self, fuerza, inteligencia, defensa):
+        self.fuerza = self.fuerza + fuerza
+        self.inteligencia = self.inteligencia + inteligencia
+        self.defensa = self.defensa + defensa
+
+    def esta_vivo(self):
+        return self.vida > 0
+    
+    def morir(self):
+        self.vida = 0
+        print(self.nombre, "ha muerto")
+    
+    def dañar(self, enemigo):
+        return self.fuerza - enemigo.defensa
+    
+    def atacar(self, enemigo):
+        daño = self.dañar(enemigo)
+        enemigo.vida = enemigo.vida - daño
+        print(self.nombre, "ah realizado", daño, "puntos de daño a", enemigo.nombre)
+        print("vida de", enemigo.nombre, "es", enemigo.vida)
 
 #Variable del constructor vacio
-mi_personaje = Personaje()
-#Modificando valores de los atributos
-mi_personaje.nombre = "EstebanDido"
-mi_personaje.fuerza = 300
-mi_personaje.inteligencia = -2
-mi_personaje.defensa = 30
-mi_personaje.vida = 2
+mi_personaje = Personaje("EstebanDido", 100, 50, 45, 100)
+mi_enemigo = Personaje("Angel", 70, 100, 40, 100)
+mi_personaje.imprimir_atributos()
+mi_personaje.atacar(mi_enemigo)
+# mi_personaje.morir()
+# print(mi_personaje.esta_vivo())
+# mi_personaje.subir_nivel(15, 5, 10)
+# print("valores actualizados")
+# mi_personaje.imprimir_atributos()
 
-print("El nombre de mi personaje es: ", mi_personaje.nombre)
-print("El nombre de mi personaje es: ", mi_personaje.fuerza)
-print("El nombre de mi personaje es: ", mi_personaje.inteligencia)
-print("El nombre de mi personaje es: ", mi_personaje.defensa)
-print("El nombre de mi personaje es: ", mi_personaje.vida)
+
+
+#Modificando valores de los atributos
+# mi_personaje.nombre = "EstebanDido"
+# mi_personaje.fuerza = 300
+# mi_personaje.inteligencia = -2
+# mi_personaje.defensa = 30
+# mi_personaje.vida = 2
+
+# print("El nombre de mi personaje es: ", mi_personaje.nombre)
+# print("El nombre de mi personaje es: ", mi_personaje.fuerza)
+# print("El nombre de mi personaje es: ", mi_personaje.inteligencia)
+# print("El nombre de mi personaje es: ", mi_personaje.defensa)
+# print("El nombre de mi personaje es: ", mi_personaje.vida)
